04a76922eb0984d413832579aa10a04bcd2af601
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2011-04-27 08:07:44 +0000

    Signing the DLL

--- Edge.Data.Pipeline/trunk/Deliveries/DeliveryDB.cs
+++ Edge.Data.Pipeline/trunk/Deliveries/DeliveryDB.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Db4objects.Db4o;
-using Db4objects.Db4o.CS;
-using Db4objects.Db4o.Config;
-using Db4objects.Db4o.Ext;
+//using Db4objects.Db4o;
+//using Db4objects.Db4o.CS;
+//using Db4objects.Db4o.Config;
+//using Db4objects.Db4o.Ext;
 
 
 
@@ -19,6 +19,12 @@ namespace Edge.Data.Pipeline.Deliveries
 		private const string _password = "123456";
 		private const int _port = 999;
 
+		public static Delivery Get(long deliveryID)
+		{
+			throw new NotImplementedException();
+		}
+
+		/*
 		public static Delivery Get(Db4oUUID deliveryID) //TODO: TALK DORON WHAT TO DO, WE HAVE TO HAVE SIGNATURE AND LONG (ID)
 		{
 			Delivery delivery;
@@ -28,8 +34,8 @@ namespace Edge.Data.Pipeline.Deliveries
 				client.Ext().Activate(delivery);
 				
 			}
-			return delivery;/*TODO: BIG PROBLEM!! IF YOU RETURN DELIVERY AFTER CLOSING THE CONNECTION(CLIENT) THE DB40 WON'T 
-			                  UPDATE ON THE SAVE, BUT IT WILL ADD MORE DELIVERY*/
+			return delivery;//TODO: BIG PROBLEM!! IF YOU RETURN DELIVERY AFTER CLOSING THE CONNECTION(CLIENT) THE DB40 WON'T 
+			                //  UPDATE ON THE SAVE, BUT IT WILL ADD MORE DELIVERY
 		}
 
 		public static void Save(Delivery delivery)
@@ -52,5 +58,6 @@ namespace Edge.Data.Pipeline.Deliveries
 				}
 			
 		}
+	*/
 	}
 }
