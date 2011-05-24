d836079095d4ff6b413c0ce19bda08ef04227522
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2011-05-24 16:39:53 +0000

    Starting working on Workflow. Defined configuration classes.

--- Edge.Core/branches/3.0.0/Services2/Workflow/WorkflowServiceConfiguration.cs
+++ Edge.Core/branches/3.0.0/Services2/Workflow/WorkflowServiceConfiguration.cs
@@ -3,20 +3,99 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Edge.Core.Services2
+namespace Edge.Core.Services2.Workflow
 {
-	/*
 	public class WorkflowServiceConfiguration: ServiceConfiguration
 	{
-		public List<WorkflowNode> Nodes;
+		public const string Self = "SELF";
+		public Group Workflow = new Group() { Name = Self, Mode = GroupMode.Linear };
 	}
-	*/
 
 	public abstract class WorkflowNode
 	{
+		public string Name;
 	}
 
-	public class ConditionNode:WorkflowNode
+	public abstract class WorkflowWorkNode:WorkflowNode
 	{
+		public WorkflowNodeFailureBehavior FailureBehavior = WorkflowNodeFailureBehavior.Terminate;
+	}
+
+	public enum WorkflowNodeFailureBehavior
+	{
+		Continue,
+		Terminate
+	}
+
+	public class Group : WorkflowWorkNode
+	{
+		public GroupMode Mode = GroupMode.Linear;
+		public List<WorkflowNode> Nodes;
+	}
+
+	public class Step : WorkflowWorkNode
+	{
+		public ServiceConfiguration Service;
+	}
+
+	public class Condition:WorkflowNode
+	{
+		public Func<WorkflowNodeInstance, bool> Test;
+		public WorkflowNode MoveTo;
+
+		public Condition(Func<WorkflowNodeInstance, bool> test)
+		{
+			Test = test;
+		}
+
+	}
+
+	public class If : Condition
+	{
+		public If(Func<WorkflowNodeInstance, bool> test)
+			: base(test)
+		{
+		}
+
+		public WorkflowNode Then;
+		public List<Condition> ElseIf;
+		public Condition Else;
+	}
+
+
+	public enum GroupMode
+	{
+		Linear,
+		Parallel
+	}
+
+	public class End: WorkflowNode
+	{
+		public End(ServiceOutcome outcome) { this.Outcome = outcome; }
+		public ServiceOutcome Outcome;
+	}
+
+	public class WorkflowNodeInstance
+	{
+		Dictionary<string, WorkflowNodeInstance> _children;
+
+		public WorkflowNodeInstance Root;
+		public WorkflowNodeInstance Parent;
+		public WorkflowNode Node;
+		public ServiceInstance Instance;
+
+		public ICollection<WorkflowNodeInstance> Children
+		{
+			get { return _children.Values; }
+		}
+
+		public WorkflowNodeInstance this[string name]
+		{
+			get { return _children[name]; }
+		}
+		public WorkflowNodeInstance this[int name]
+		{
+			get { return _children[name.ToString()]; }
+		}
 	}
 }
