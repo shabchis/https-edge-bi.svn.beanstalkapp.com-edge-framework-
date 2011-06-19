a2102da15f5f6d35478d940a9ce9d550a9478b4e
alonyaari <alonyaari@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2011-06-19 12:06:26 +0000

    1.Implement DateTimeRange.ToString() + DateTimeRange.Parse

---
+++ Edge.Data.Pipeline/trunk/Services/ReRunService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Core.Services;
+using Edge.Core.Utilities;
+using Edge.Core.Scheduling;
+using Edge.Core;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace Edge.Data.Pipeline.Services
+{
+	class ReRunService : PipelineService	
+	{
+		protected override Core.Services.ServiceOutcome DoPipelineWork()
+		{
+
+			using (ServiceClient<IScheduleManager> scheduleManager = new ServiceClient<IScheduleManager>())
+			{
+				string targetPeriod = this.TargetPeriod.ToString();
+				//Parso json
+				JObject jObjecttimeRange = JObject.Parse(targetPeriod);
+				DateTime fromDate = (DateTime)JsonConvert.DeserializeObject<DateTime>(jObjecttimeRange["start"].ToString());
+				DateTime toDate = (DateTime)JsonConvert.DeserializeObject<DateTime>(jObjecttimeRange["end"].ToString()); 
+				string serviceName=this.Delivery.Parameters["ServiceToRun"].ToString();
+				
+				while (fromDate<=toDate)
+				{
+
+					//run the service
+					scheduleManager.Service.AddToSchedule(serviceName,this.Delivery.Account.ID,DateTime.Now,new SettingsCollection(String.Format("TargetPeriod: {0}", DayCode.ToDayCode(fromDate))));
+
+					fromDate = fromDate.AddDays(1);
+
+
+					
+
+				}
+
+			}
+			
+			return Core.Services.ServiceOutcome.Success;
+		}
+	}
+}
