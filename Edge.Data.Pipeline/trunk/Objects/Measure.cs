b0157c09ea96450e47dad70e6a00b7e2052d7697
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2011-06-19 14:46:17 +0000

    Expanded measure handling

--- Edge.Data.Pipeline/trunk/Objects/Measure.cs
+++ Edge.Data.Pipeline/trunk/Objects/Measure.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data.SqlClient;
+using Edge.Core.Data;
 
 namespace Edge.Data.Objects
 {
@@ -10,13 +12,68 @@ namespace Edge.Data.Objects
 		public int ID;
 		public Account Account;
 		public string Name;
-		public int DeliveryColumnIndex;
+		public MeasureType MeasureType;
+		public string OltpName;
+		public string DisplayName;
 
-		public static readonly Measure Cost					= new Measure() { ID = -601, DeliveryColumnIndex = 1 };
-		public static readonly Measure Impressions			= new Measure() { ID = -602, DeliveryColumnIndex = 2 };
-		public static readonly Measure UniqueImpressions	= new Measure() { ID = -603, DeliveryColumnIndex = 3 };
-		public static readonly Measure Clicks				= new Measure() { ID = -604, DeliveryColumnIndex = 4 };
-		public static readonly Measure UniqueClicks			= new Measure() { ID = -605, DeliveryColumnIndex = 5 };
-		public static readonly Measure AveragePosition		= new Measure() { ID = -606, DeliveryColumnIndex = 6 };
+		public static readonly Measure Cost					= new Measure() { ID = -601};
+		public static readonly Measure Impressions			= new Measure() { ID = -602};
+		public static readonly Measure UniqueImpressions	= new Measure() { ID = -603};
+		public static readonly Measure Clicks				= new Measure() { ID = -604};
+		public static readonly Measure UniqueClicks			= new Measure() { ID = -605};
+		public static readonly Measure AveragePosition		= new Measure() { ID = -606};
+
+		public static Measure[] GetMeasuresForAccount(Account account, SqlConnection connection)
+		{
+			SqlCommand cmd = DataManager.CreateCommand(@"Measure_GetMeasure(
+				@accountID:Int,
+				@measureID:Int,
+				@includeBase:bit,
+				@flags:int",
+			System.Data.CommandType.StoredProcedure);
+			cmd.Connection = connection;
+
+			cmd.Parameters["@accountID"].Value = account.ID;
+			cmd.Parameters["@flags"].Value = (int)MeasureOption.All - MeasureOption.IsTarget;
+
+			List<Measure> measures = new List<Measure>();
+			using (SqlDataReader reader = cmd.ExecuteReader())
+			{
+				while (reader.Read())
+				{
+					Measure m = new Measure()
+					{
+						ID = (int) reader["MeasureID"],
+						Account = account,
+						Name = (string) reader["MeasureName"],
+						OltpName = (string) reader["FieldName"],
+						MeasureType = (MeasureType) reader["MeasureType"]
+					};
+
+					measures.Add(m);
+				}
+			}
+
+			return measures.ToArray();
+		}
+
+		[Flags]
+		enum MeasureOption
+		{
+			IsTarget = 0x2,
+			All = 0xff
+		}
 	}
+
+	public enum MeasureType
+	{
+		Cost,	
+		Impressions,
+		UniqueImpressions,
+		Clicks,	
+		UniqueClicks,
+		AveragePosition,
+		Other
+	}
+
 }
