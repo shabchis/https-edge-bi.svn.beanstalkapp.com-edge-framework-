2485a05d9427d01caccadc1523a69a668293d708
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2011-06-22 17:58:57 +0000

    Auto segmnetation

---
+++ Edge.Data.Pipeline/trunk/Configuration/RegexElementCollection.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Core.Configuration;
+using System.Configuration;
+
+namespace Edge.Data.Pipeline.Configuration
+{
+	public class RegexElementCollection : ConfigurationElementCollectionBase<RegexElement>
+	{
+		public override ConfigurationElementCollectionType CollectionType
+		{
+			get { return ConfigurationElementCollectionType.BasicMap; }
+		}
+
+		protected override string ElementName
+		{
+			get { return "Regex"; }
+		}
+
+		protected override ConfigurationElement CreateNewElement()
+		{
+			return new RegexElement();
+		}
+
+		protected override object GetElementKey(ConfigurationElement element)
+		{
+			// Get index name
+			return (element as RegexElement).Pattern;
+		}
+	}
+
+	public class RegexElement:ConfigurationElement
+	{
+		[ConfigurationProperty("Pattern", IsRequired=true, IsKey=true)]
+		public string Pattern
+		{
+			get { return (string)this["Pattern"]; }
+			set { this["Pattern"] = value; }
+		}
+	}
+
+}
--- Edge.Data.Pipeline/trunk/Objects/Tracker.cs
+++ Edge.Data.Pipeline/trunk/Objects/Tracker.cs
@@ -2,9 +2,96 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using Edge.Data.Objects;
 
-namespace Edge.Data.Objects
+namespace Edge.Data.Pipeline
 {
+
+	public class AutoSegmentationEngine
+	{
+		public Regex[] Patterns {get; set;}
+		public AutoSegmentationDefinition[] Definitions {get; set;}
+		public Func<Segment, Dictionary<string, string>, SegmentValue> OnCreateSegmentValues { get; set; }
+
+		public void Begin()
+		{
+			foreach (AutoSegmentationDefinition def in this.Definitions)
+			{
+				def.TempValues = new Dictionary<string, string>();
+				foreach (string param in def.RequiredParameters)
+					def.TempValues.Add(param, null);
+			}
+		}
+
+		// Performance is key! will be run millions of times a day.
+		public Dictionary<Segment, SegmentValue> ExtractSegmentsFromString(string source)
+		{
+			var output = new Dictionary<Segment, SegmentValue>();
+
+			for (int r = 0; r < this.Patterns.Length; r++ )
+			{
+				Regex regex = this.Patterns[r];
+				Match match = regex.Match(source);
+				if (!match.Success)
+					continue;
+
+				// Find a definition that works
+				for (int d = 0; d < this.Definitions.Length; d++)
+				{
+					AutoSegmentationDefinition def = this.Definitions[d];
+
+					// Make sure all required parameters are present, and save them to temp. values if they are
+					if (!def.RequiredParameters.All(param => {
+						Group gr = match.Groups[param];
+						bool success = gr != null && gr.Success;
+						if (success)
+							def.TempValues[param] = gr.Value;
+						return success;
+					}))
+						continue;
+
+					SegmentValue value = CreateSegmentValue(def.Segment, def.TempValues);
+					if (value != null)
+						output[def.Segment] = value;
+				}
+
+				// Break because we don't want to match more than one pattern
+				break;
+			}
+
+			return output;
+		}
+
+		private StringBuilder _stringBuilder = new StringBuilder();
+		private SegmentValue CreateSegmentValue(Segment segment, Dictionary<string, string> values)
+		{
+			// Custom segment generation
+			if (OnCreateSegmentValues != null)
+				return OnCreateSegmentValues(segment, values);
+			return null;
+			/*
+			_stringBuilder.Clear();
+			foreach (var pair in values)
+			{
+				_stringBuilder.AppendFormat("{0}={1}__"
+			}
+			*/
+		}
+	}
+
+	public class AutoSegmentationDefinition
+	{
+		public string[] RequiredParameters { get; set; }
+		public Segment Segment {get; set;}
+
+		// For internal, temporary use only.
+		internal Dictionary<string, string> TempValues;
+
+	}
+
+	#region Made obsolete by shifting to segments
+	/*
 	public class Tracker
 	{
 		public Dictionary<string,string> Values { get; set; }
@@ -45,4 +132,7 @@ namespace Edge.Data.Objects
 			Ad = ad;
 		}
 	}
+	*/
+	#endregion
+
 }
