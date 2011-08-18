1f9341d22617c721ae50da4f114a5c53f6e41486
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2011-08-18 15:54:19 +0000

    Added more complex conflict handling in PipelineService.HandleConflicts. NEEDS TO BE CHECKED

--- Edge.Data.Pipeline/trunk/Services/PipelineService.cs
+++ Edge.Data.Pipeline/trunk/Services/PipelineService.cs
@@ -142,27 +142,49 @@ namespace Edge.Data.Pipeline.Services
 			conflicting.CopyTo(toCheck, 1);
 
 			// Check whether the last commit was not rolled back for each conflicting delivery
+			bool conflictsFound = false;
+			List<Delivery> inProcess = new List<Delivery>();
 			List<Delivery> toRollback = new List<Delivery>();
+
 			foreach (Delivery d in toCheck)
 			{
 				int rollbackIndex = -1;
 				int commitIndex = -1;
+				int abortedIndex = -1;
+				int otherIndex = -1;
+
 				for (int i = 0; i < d.History.Count; i++)
 				{
 					if (d.History[i].Operation == DeliveryOperation.Committed)
 						commitIndex = i;
 					else if (d.History[i].Operation == DeliveryOperation.RolledBack)
 						rollbackIndex = i;
+					else if (d.History[i].Operation == DeliveryOperation.Aborted)
+						abortedIndex = i;
+					else
+						otherIndex = i;
 				}
 
+				// Conflicts means either an unrolled back commit or the last operation not being an abort
+
+
 				if (commitIndex > rollbackIndex)
+				{
 					toRollback.Add(d);
+					conflictsFound = true;
+				}
+
+				if (otherIndex > abortedIndex && otherIndex > commitIndex)
+				{
+					inProcess.Add(d);
+					conflictsFound = true;
+				}
 			}
 
 			DeliveryRollbackOperation operation = null;
-			if (toRollback.Count > 0)
+			if (conflictsFound)
 			{
-				if (behavior == DeliveryConflictBehavior.Rollback)
+				if (behavior == DeliveryConflictBehavior.Rollback && inProcess.Count == 0)
 				{
 					operation = new DeliveryRollbackOperation();
 					operation.AsyncDelegate = new Action<Delivery[]>(importManager.Rollback);
@@ -170,19 +192,24 @@ namespace Edge.Data.Pipeline.Services
 				}
 				else
 				{
+					var guidOutputList = new List<Delivery>(inProcess);
+					guidOutputList.AddRange(toRollback);
+
 					StringBuilder guids = new StringBuilder();
-					for (int i = 0; i < toRollback.Count; i++)
+					for (int i = 0; i < guidOutputList.Count; i++)
 					{
-						guids.Append(toRollback[i].DeliveryID.ToString("N"));
-						if (i < toRollback.Count - 1)
+						guids.Append(guidOutputList[i].DeliveryID.ToString("N"));
+						if (i < guidOutputList.Count - 1)
 							guids.Append(", ");
 					}
+
 					throw new Exception("Conflicting deliveries found: " + guids.ToString());
 				}
 			}
 
 			return operation;
 		}
+
 
 		// ==============================
 		#endregion
