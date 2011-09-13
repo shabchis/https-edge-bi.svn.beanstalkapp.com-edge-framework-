a340aee88ac4546aeed1cdbbe227cda90799de14
alonyaari <alonyaari@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2011-09-13 13:12:22 +0000

    More of fuck db40

--- Edge.Data.Pipeline/branches/v2.9.1/Deliveries/DeliveryDB.cs
+++ Edge.Data.Pipeline/branches/v2.9.1/Deliveries/DeliveryDB.cs
@@ -36,7 +36,7 @@ namespace Edge.Data.Pipeline
 
 		internal static Delivery Get(Guid deliveryID, bool deep = true, SqlConnection connection = null)
 		{
-			Delivery delivery;
+			Delivery delivery=null;
 			bool innerConnection = connection == null;
 
 			if (innerConnection)
@@ -50,32 +50,102 @@ namespace Edge.Data.Pipeline
 
 				using (SqlDataReader reader = cmd.ExecuteReader())
 				{
-					int resultSetCount=0;
-					while (reader.NextResult())
+
+					while (reader.Read())
 					{
-						resultSetCount++;
-						switch (resultSetCount)
+
+						//**********************Delivery*********************************
+						delivery = new Delivery(reader.Convert<string, Guid>("DeliveryID", s => Guid.Parse(s)));
+
+						delivery.Account = reader.Convert<int?, Account>("AccountID", id => id.HasValue ? new Account() { ID = id.Value } : null);
+						delivery.Channel = reader.Convert<int?, Channel>("ChannelID", id => id.HasValue ? new Channel() { ID = id.Value } : null);
+						//delivery.DateCreated=DateTime.Parse(reader["DateCreated"].ToString());
+						//delivery.DateModified=DateTime.Parse(reader["DateModified"].ToString());
+						delivery.Signature = reader["Signature"].ToString();
+						delivery.Description = reader["Description"].ToString();
+						delivery.TargetLocationDirectory = reader["TargetLocationDirectory"].ToString();
+						delivery.TargetPeriod = DateTimeRange.Parse(reader["TargetPeriodDefinition"].ToString());
+						//delivery.TargetPeriodStart=DateTime.Parse(reader["TargetPeriodStart"].ToString());
+						//delivery.TargetPeriodEnd=DateTime.Parse(reader["TargetPeriodEnd"].ToString());
+
+						//*******************DeliveryParameters***********************
+
+						if (reader.NextResult())
 						{
-							case ResultSetIndex.Delivery:
-								delivery = new Delivery(reader.Convert<string,Guid>("DeliveryID", s => Guid.Parse(s)))
-								{
-									Account = reader.Convert<int?,Account>("AccountID", id => id.HasValue ? new Account(){ID = id.Value} : null),
-									Channel = reader.Convert<int?, Channel>("ChannelID", id => id.HasValue ? new Channel() { ID = id.Value } : null),
-								};
-								break;
-							case ResultSetIndex.DeliveryParameters:
-								break;
-							case ResultSetIndex.DeliveryHistory:
-								break;
-							case ResultSetIndex.DeliveryHistoryParameters:
-								break;
-							case ResultSetIndex.DeliveryFile:
-								break;
-							case ResultSetIndex.DeliveryFileParameters:
-								break;
+							while (reader.Read())
+								delivery.Parameters.Add(reader["Key"].ToString(), reader.Get<object>("Value"));
 						}
+						//*******************DeliveryHistory***********************
+						if (reader.NextResult())
+						{
+							while (reader.Read())
+								delivery.History.Add(new DeliveryHistoryEntry((DeliveryOperation)reader["Operation"], Convert.ToInt64(reader["ServiceInstanceID"])));							
+						}
+
+						//*******************DeliveryHistoryParameters***********************
+						if (reader.NextResult())
+						{
+							while (reader.Read())
+								delivery.History[reader.Get<int>("Index")].Parameters.Add(reader["Key"].ToString(), reader.Get<object>("Value"));
+						}
+						//*******************DeliveryFile***********************
+						if (reader.NextResult())
+						{
+							while (reader.Read())
+							{
+								DeliveryFile deliveryFile = new DeliveryFile();
+								deliveryFile.Account = reader.Convert<int?, Account>("AccountID", id => id.HasValue ? new Account() { ID = id.Value } : null);
+								deliveryFile.FileID = reader.Convert<string, Guid>("DeliveryID", s => Guid.Parse(s));
+								deliveryFile.FileFormat = (FileCompression)reader["FileCompression"];
+								deliveryFile.SourceUrl = reader["SourceUrl"].ToString();
+								deliveryFile.Name = reader["Name"].ToString();
+								deliveryFile.Location = reader["Location"].ToString();
+								delivery.Files.Add(deliveryFile);
+							}
+
+						}
+						//*******************DeliveryFileParameters***********************
+						if (reader.NextResult())
+						{
+							while (reader.Read())
+							{
+								DeliveryFile deliveryFile=delivery.Files[reader["FileID"].ToString()];
+								deliveryFile.Parameters.Add(reader["Key"].ToString(), reader.Get<object>("Value"));
+								
+								
+							}
+
+						}
+						//*******************DeliveryFileHistory***********************
+						if (reader.NextResult())
+						{
+							while (reader.Read())
+							{
+								DeliveryFile deliveryFile = delivery.Files[reader["FileID"].ToString()];
+								deliveryFile.History.Add(new DeliveryHistoryEntry((DeliveryOperation)reader["Operation"], Convert.ToInt64(reader["ServiceInstanceID"])));	
+							}
+
+
+						}
+						//*******************DeliveryFileHistoryParameters***********************
+						if (reader.NextResult())
+						{
+							while (reader.Read())
+							{
+								DeliveryFile deliveryFile = delivery.Files[reader["FileID"].ToString()];
+								deliveryFile.History[reader.Get<int>("Index")].Parameters.Add(reader["Key"].ToString(), reader.Get<object>("Value"));
+							}
+
+
+						}
+
+
+
+						
+						
 					}
 				}
+
 			}
 			finally
 			{
@@ -84,18 +154,18 @@ namespace Edge.Data.Pipeline
 					connection.Dispose();
 				}
 			}
-/*
-#if DEBUG
-			Log.Write(String.Format("Delivery - {3}found: {0} (activate: {2}, {1} results)\n", deliveryID, resultCount, activate, delivery == null ? "not " : "" ), LogMessageType.Information);
+			/*
+			#if DEBUG
+						Log.Write(String.Format("Delivery - {3}found: {0} (activate: {2}, {1} results)\n", deliveryID, resultCount, activate, delivery == null ? "not " : "" ), LogMessageType.Information);
 
-#endif			
-*/
+			#endif			
+			*/
 			return delivery;
 		}
 
 		internal static Guid Save(Delivery delivery)
 		{
-			
+
 			using (var client = DeliveryDBClient.Connect())
 			{
 				Guid guid = delivery.DeliveryID;
@@ -107,8 +177,8 @@ namespace Edge.Data.Pipeline
 					{
 						// refer here for an explanation on what's going on:
 						// http://stackoverflow.com/questions/5848990/retrieve-an-object-in-one-db4o-session-store-in-another-disconnected-scenario
-						long tempDb4oID = client.Ext().GetID(inactiveReference);
-						client.Ext().Bind(delivery, tempDb4oID);
+						//long tempDb4oID = client.Ext().GetID(inactiveReference);
+						//client.Ext().Bind(delivery, tempDb4oID);
 						//Log.Write(String.Format("Delivery - overwrite (db4o id {0})", tempDb4oID), LogMessageType.Information);
 					}
 				}
@@ -125,61 +195,69 @@ namespace Edge.Data.Pipeline
 
 				// Try to store, and return new guid on success
 
-				client.Store(delivery);
+				//client.Store(delivery);
 				return guid;
 			}
 		}
 
 		internal static void Delete(Delivery delivery, IObjectContainer client = null)
 		{
-			bool close = true;
-			if (client == null)
-				client = DeliveryDBClient.Connect();
-			else
-				close = false;
-
-			try
-			{
-				
-				// Get an inactive reference, get its temp ID and bind the supplied object to it instead
-				Delivery inactiveReference = DeliveryDB.Get(delivery.DeliveryID, false, client);
-				if (inactiveReference != null) {
-					client.Delete(inactiveReference);
-				}
-			}
-			finally
-			{
-				if (close)
-					client.Dispose();
-			}
+			throw new NotImplementedException();
 		}
 
 		internal static Delivery[] GetBySignature(string signature, Guid[] exclude)
 		{
+			List<Delivery> deliveries = new List<Delivery>();
 			using (var client = DeliveryDBClient.Connect())
 			{
 				// Select deliveries that match a signature but none of the guids in 'exclude'
-				var results = from Delivery d in client
-							  where d.Signature == signature && !exclude.Any(toExclude => d.DeliveryID == toExclude)
-							 select d;
-
-				return results.ToArray();
+				using (SqlCommand cmd=DataManager.CreateCommand("Delivery_GetBySignature(@signature:NvarChar,@exclude:NvarChar)",System.Data.CommandType.StoredProcedure))
+				{
+					cmd.Connection = client;
+					cmd.Parameters["@signature"].Value = signature;
+					cmd.Parameters["@exclude"].Value = GetGuidStringArray(exclude);
+					using (SqlDataReader reader=cmd.ExecuteReader())
+					{						
+						while (reader.Read())						
+							deliveries.Add(Get(Guid.Parse(reader.GetString(0))));
+					}
+				}
 			}
+			return deliveries.ToArray();
+		}
+
+		private static string GetGuidStringArray(Guid[] exclude)
+		{
+			StringBuilder guidArray=new StringBuilder();
+			foreach (Guid guid in exclude)
+				guidArray.AppendFormat(guidArray.Length == 0 ? "{0}" : ",{0}", guid.ToString("N"));
+
+			return guidArray.ToString();
 		}
 
 		internal static Delivery[] GetByTargetPeriod(int channelID, int accountID, DateTime start, DateTime end)
 		{
+			List<Delivery> deliveries = new List<Delivery>();
 			using (var client = DeliveryDBClient.Connect())
 			{
-				var results = from Delivery d in client
-							  where d.Channel.ID == channelID
-							  select d;
-
-				return results.ToArray();
+				using (SqlCommand cmd = DataManager.CreateCommand("Delivery_GetByTargetPeriod(@channelID:Int,@accountID:Int,@targetPeriodStart:DateTime,@targetPeriodEnd:DateTime)", System.Data.CommandType.StoredProcedure))
+				{
+					cmd.Connection = client;
+					cmd.Parameters["@channelID"].Value = channelID;
+					cmd.Parameters["@accountID"].Value = accountID;
+					cmd.Parameters["@targetPeriodStart"].Value = start;
+					cmd.Parameters["@targetPeriodEnd"].Value = end;
+					using (SqlDataReader reader = cmd.ExecuteReader())
+					{
+						while (reader.Read())
+							deliveries.Add(Get(Guid.Parse(reader.GetString(0))));
+					}
+				}
 			}
+			return deliveries.ToArray();
 		}
 
-		
+
 	}
 
 	internal static class DeliveryDBClient
