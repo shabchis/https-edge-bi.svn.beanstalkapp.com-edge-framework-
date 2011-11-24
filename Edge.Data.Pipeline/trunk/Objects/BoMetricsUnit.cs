27dc8531c02472c143d07a47bc21c4f32141245e
sbarchen <sbarchen@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2011-11-24 12:31:43 +0000

    Added BOMetrics

---
+++ Edge.Data.Pipeline/trunk/Objects/BoMetricsUnit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Pipeline;
+
+namespace Edge.Data.Objects
+{
+	public class BoMetricsUnit
+	{
+		public readonly Guid Guid = Guid.NewGuid();
+
+		public DateTime PeriodStart;
+		public DateTime PeriodEnd;
+
+		public Dictionary<Segment, SegmentValue> Segments = new Dictionary<Segment, SegmentValue>();
+
+		public Dictionary<Measure, double> MeasureValues=new Dictionary<Measure,double>();
+
+	}
+}
