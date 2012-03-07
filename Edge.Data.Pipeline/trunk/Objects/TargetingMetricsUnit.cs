b51cad9a9829bbf0b83fbd7ea381a25c3683b14c
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2012-03-07 13:10:35 +0000

    Finished adding TargetingMetrics project

--- Edge.Data.Pipeline/trunk/Objects/TargetingMetricsUnit.cs
+++ Edge.Data.Pipeline/trunk/Objects/TargetingMetricsUnit.cs
@@ -6,16 +6,16 @@ using Edge.Data.Pipeline;
 
 namespace Edge.Data.Objects
 {
-	public class SegmentMetricsUnit
+	public class TargetingMetricsUnit
 	{
-		public readonly Guid Guid = Guid.NewGuid();
+		public readonly Guid Usid = Guid.NewGuid();
 
 		public DateTime PeriodStart;
 		public DateTime PeriodEnd;
-
+		
 		public Currency Currency;
-
-		public Dictionary<Segment, SegmentValue> Segments = new Dictionary<Segment, SegmentValue>();
+		
+		public List<Target> TargetMatches = new List<Target>();
 
 		public Dictionary<Measure, double> MeasureValues=new Dictionary<Measure,double>();
 
