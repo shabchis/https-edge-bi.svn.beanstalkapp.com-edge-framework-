8e524ca944e953d1e3b74c1c87f099e8c8058427
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2012-05-02 14:18:06 +0000

    Finished moving around metrics processers, created reader adapters

---
+++ Edge.Data.Pipeline/branches/importMappings/Readers/Base/ReaderAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Edge.Core.Configuration;
+
+namespace Edge.Data.Pipeline
+{
+	public abstract class ReaderAdapter: IDisposable
+	{
+		public IReader Reader { get; protected set; }
+
+		public abstract void Init(Stream stream, ServiceElement configuration);
+		public abstract object GetField(string field);
+
+		public void Dispose()
+		{
+			if (this.Reader != null)
+				this.Reader.Dispose();
+		}
+	}
+}
---
+++ Edge.Data.Pipeline/branches/importMappings/Readers/CsvDynamicReaderAdapter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Core.Configuration;
+using System.IO;
+
+namespace Edge.Data.Pipeline
+{
+	public class CsvProcessorService : ReaderAdapter
+	{
+		string[] _requiredColumns;
+		string _delimeter;
+		Encoding _encoding;
+
+		public override void Init(Stream stream, ServiceElement configuration)
+		{
+			_requiredColumns = configuration.GetOption("CsvRequiredColumns").Split();
+			_delimeter = configuration.Options["CsvDelimeter"];
+			string encoding = configuration.Options["CsvEncoding"];
+			if (!String.IsNullOrEmpty(encoding))
+				_encoding = Encoding.GetEncoding(encoding);
+
+			base.Reader = new CsvDynamicReader(stream, _requiredColumns);
+		}
+
+		public new CsvDynamicReader Reader
+		{
+			get { return (CsvDynamicReader)base.Reader; }
+		}
+
+		public override object GetField(string field)
+		{
+			return this.Reader.Current[field];
+		}
+	}
+}
