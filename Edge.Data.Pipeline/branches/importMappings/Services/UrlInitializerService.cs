fffe0912269888a5856760d2c446bd8e3354a254
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2012-05-15 07:42:28 +0000

    Changed generic initializer/retriever to UrlInitialier/retriever to avoid confusion with generic metrics.

---
+++ Edge.Data.Pipeline/branches/importMappings/Services/UrlInitializerService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Pipeline;
+using Edge.Data.Pipeline.Services;
+using Edge.Data.Objects;
+
+namespace Edge.Data.Pipeline.Services
+{
+	public class UrlInitializerService: PipelineService
+	{
+		protected override Core.Services.ServiceOutcome DoPipelineWork()
+		{
+			if (this.Delivery == null)
+			{
+				this.Delivery = NewDelivery();
+				this.Delivery.TargetPeriod = this.TargetPeriod;
+				this.Delivery.Account = this.Instance.AccountID != -1 ? new Account() { ID = this.Instance.AccountID } : null; // no account means there is no permission validation
+				this.Delivery.TargetLocationDirectory = this.Instance.Configuration.GetOption(Const.DeliveryServiceConfigurationOptions.TargetLocationDirectory);
+			}
+
+			this.Delivery.Files.Add(new DeliveryFile()
+			{
+				Name = this.Instance.Configuration.Options[Const.DeliveryServiceConfigurationOptions.DeliveryFileName],
+				SourceUrl = this.Instance.Configuration.GetOption(Const.DeliveryServiceConfigurationOptions.SourceUrl)
+			});
+
+			this.Delivery.Save();
+
+			return Core.Services.ServiceOutcome.Success;
+		}
+	}
+}
