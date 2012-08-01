c15f88be377871f07105a5ce2ae77bc52d599ba3
sbarchen <sbarchen@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2012-08-01 11:35:49 +0000

    Currency Object update

--- Edge.Data.Pipeline/trunk/Objects/Currency.cs
+++ Edge.Data.Pipeline/trunk/Objects/Currency.cs
@@ -5,16 +5,24 @@ using System.Text;
 
 namespace Edge.Data.Objects
 {
-	public class Currency
-	{
-		public string Code;
-	}
-
 	public class CurrencyRate
 	{
 		public Currency Currency;
 		public DateTime RateDate;
 		public double RateValue;
 		public DateTime DateCreated;
+
+		public CurrencyRate()
+		{
+			this.Currency = new Currency();
+			this.DateCreated = DateTime.Today;
+		}
 	}
+	
+	public class Currency
+	{
+		public string Code;
+	}
+
+	
 }
