13cebaeba7eceb89a3c4a7feccb7466522972e43
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2012-08-06 11:35:28 +0000

    Scheduler fixes with Alon

--- Edge.Core.Scheduling/branches/2.9.1/Objects/SchedulingRequest.cs
+++ Edge.Core.Scheduling/branches/2.9.1/Objects/SchedulingRequest.cs
@@ -36,6 +36,7 @@ namespace Edge.Core.Scheduling.Objects
 					return null;
 			}
 		}
+
 		public string UniqueKey
 		{
 			get
@@ -43,70 +44,75 @@ namespace Edge.Core.Scheduling.Objects
 				return String.Format("profile:{0},base:{1},name:{2},scope:{3},time:{4}", this.Configuration.Profile.ID, Configuration.BaseConfiguration.Name, Configuration.Name, Rule.Scope, RequestedTime);
 			}
 		}
-		private bool _saved;
+
+		private bool _saved = false;
+		private object _saveLock = new object();
 		public SchedulingStatus SchedulingStatus { get;  set; }
 		
 
 		public void Save()
 		{
-			using (SqlConnection conn = new SqlConnection(AppSettings.GetConnectionString("Edge.Core.Services", "SystemDatabase")))
+			lock (_saveLock)
 			{
-				SqlCommand command;
-				if (!_saved)
+				using (SqlConnection conn = new SqlConnection(AppSettings.GetConnectionString("Edge.Core.Services", "SystemDatabase")))
 				{
-					command = DataManager.CreateCommand(@"INSERT INTO [Edge_System291].[dbo].[Scheduling]
-           ([RequestID]
-           ,[UniqueKey]
-           ,[RequestedTime]
-           ,[InstanceName]
-           ,[InstanceUses]
-           ,[LegacyInstanceID]
-           ,[Outcome]
-		   ,[SchedulingScope]
-		   ,[SchedulingStatus]
-		   ,[ExpectedStartTime])
-     VALUES
-           (@RequestID:char,
-           @UniqueKey:nvarchar,
-           @RequestedTime:datetime,
-           @InstanceName:nvarchar,
-           @InstanceUses:nvarchar,
-           @LegacyInstanceID:bigint,
-           @Outcome:int,
-		   @SchedulingScope:int,
-		   @SchedulingStatus:int,
-		   @ExpectedStartTime:datetime)");
+					SqlCommand command;
+					if (!_saved)
+					{
+						command = DataManager.CreateCommand(@"INSERT INTO [Scheduling]
+						   ([RequestID]
+						   ,[UniqueKey]
+						   ,[RequestedTime]
+						   ,[InstanceName]
+						   ,[InstanceUses]
+						   ,[LegacyInstanceID]
+						   ,[Outcome]
+						   ,[SchedulingScope]
+						   ,[SchedulingStatus]
+						   ,[ExpectedStartTime])
+					 VALUES
+						   (@RequestID:char,
+						   @UniqueKey:nvarchar,
+						   @RequestedTime:datetime,
+						   @InstanceName:nvarchar,
+						   @InstanceUses:nvarchar,
+						   @LegacyInstanceID:bigint,
+						   @Outcome:int,
+						   @SchedulingScope:int,
+						   @SchedulingStatus:int,
+						   @ExpectedStartTime:datetime)");
+					}
+					else
+					{
+						command = DataManager.CreateCommand(@"UPDATE [Scheduling]
+						SET
+							[UniqueKey]=@UniqueKey:nvarchar
+						   ,[RequestedTime]=@RequestedTime:datetime
+						   ,[InstanceName]=@InstanceName:nvarchar
+						   ,[InstanceUses]=@InstanceUses:nvarchar
+						   ,[LegacyInstanceID]=@LegacyInstanceID:bigint
+						   ,[Outcome]=@Outcome:int
+						   ,[SchedulingScope]=@SchedulingScope:int
+						   ,[SchedulingStatus]=@SchedulingStatus:int
+						   ,[ExpectedStartTime]=@ExpectedStartTime:datetime
+							WHERE RequestID=@RequestID:char");
+					}
+					command.Parameters["@UniqueKey"].Value = this.UniqueKey;
+					command.Parameters["@RequestedTime"].Value = this.RequestedTime;
+					command.Parameters["@InstanceName"].Value = this.Configuration.Name;
+					command.Parameters["@InstanceUses"].Value = this.Configuration.BaseConfiguration.Name;
+					command.Parameters["@LegacyInstanceID"].Value = this.Instance.LegacyInstance.InstanceID;
+					command.Parameters["@Outcome"].Value = this.Instance.Outcome;
+					command.Parameters["@RequestID"].Value = this.RequestID.ToString("N");
+					command.Parameters["@SchedulingStatus"].Value = this.SchedulingStatus;
+					command.Parameters["@SchedulingScope"].Value = this.Rule.Scope;
+					command.Parameters["@ExpectedStartTime"].Value = this.Instance.ExpectedStartTime == DateTime.MinValue ? (object)DBNull.Value : this.Instance.ExpectedStartTime;
+					conn.Open();
+					command.Connection = conn;
+					if (command.ExecuteNonQuery() < 0)
+						throw new Exception("Scheduling Request not saved");
+					_saved = true;
 				}
-				else
-				{
-					command = DataManager.CreateCommand(@"UPDATE [Edge_System291].[dbo].[Scheduling]
-					SET
-			[UniqueKey]=@UniqueKey:nvarchar
-           ,[RequestedTime]=@RequestedTime:datetime
-           ,[InstanceName]=@InstanceName:nvarchar
-           ,[InstanceUses]=@InstanceUses:nvarchar
-           ,[LegacyInstanceID]=@LegacyInstanceID:bigint
-           ,[Outcome]=@Outcome:int
-		   ,[SchedulingScope]=@SchedulingScope:int
-		   ,[SchedulingStatus]=@SchedulingStatus:int
-		   ,[ExpectedStartTime]=@ExpectedStartTime:datetime
-			WHERE RequestID=@RequestID:char");
-				}
-				command.Parameters["@UniqueKey"].Value = this.UniqueKey;
-				command.Parameters["@RequestedTime"].Value = this.RequestedTime;
-				command.Parameters["@InstanceName"].Value = this.Configuration.Name;
-				command.Parameters["@InstanceUses"].Value = this.Configuration.BaseConfiguration.Name;
-				command.Parameters["@LegacyInstanceID"].Value = this.Instance.LegacyInstance.InstanceID;
-				command.Parameters["@Outcome"].Value = this.Instance.Outcome;
-				command.Parameters["@RequestID"].Value = this.RequestID.ToString("N");
-				command.Parameters["@SchedulingStatus"].Value=this.SchedulingStatus;
-				command.Parameters["@SchedulingScope"].Value = this.Rule.Scope;
-				command.Parameters["@ExpectedStartTime"].Value = this.Instance.ExpectedStartTime == DateTime.MinValue ? (object)DBNull.Value : this.Instance.ExpectedStartTime;
-				conn.Open();
-				command.Connection = conn;
-				if (command.ExecuteNonQuery() < 0)
-					throw new Exception("Scheduling Request not saved");
-				_saved = true;
 			}
 		}
 
@@ -157,6 +163,6 @@ namespace Edge.Core.Scheduling.Objects
 		Request,
 		Scheduled,
 		Done,
-		WillNotRun
+		CouldNotBeScheduled
 	}
 }
