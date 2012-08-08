529916ee547d21b90f46d4bbd6605b1ee875bbd9
alonyaari <alonyaari@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2012-08-08 07:59:09 +0000

    STARTED TO WRITE ROLEBACK NOT READY YET

--- Edge.Data.Pipeline.Metrics/trunk/Services/MetricsRollbackService.cs
+++ Edge.Data.Pipeline.Metrics/trunk/Services/MetricsRollbackService.cs
@@ -71,6 +71,74 @@ namespace Edge.Data.Pipeline.Metrics.Services
 
 
 			return Core.Services.ServiceOutcome.Success;
+
+			///for new db
+
+			/*string[] deliveriesIds = null;
+			string[] ouputsIds = null;
+			if (Instance.Configuration.Options.ContainsKey(Consts.ConfigurationOptions.RollbackDeliveries))
+				deliveriesIds = this.Instance.Configuration.GetOption(Consts.ConfigurationOptions.RollbackDeliveries).Split(',');
+			else if (Instance.Configuration.Options.ContainsKey(Consts.ConfigurationOptions.RollbackOutputs))
+				ouputsIds = this.Instance.Configuration.GetOption(Consts.ConfigurationOptions.RollbackOutputs).Split(',');
+			else
+				throw new Exception("Option RollbackDeliveries or RollbackOutputs must be defined");
+			string spRolebackbyDeliveries = this.Instance.Configuration.GetOption(Consts.ConfigurationOptions.RollbackByDeliverisStoredProc);
+			string spRolebackbyOutputs = this.Instance.Configuration.GetOption(Consts.ConfigurationOptions.RollbackByOutputsStoredProc);
+			string tableName = this.Instance.Configuration.GetOption(Consts.ConfigurationOptions.RollbackTableName);
+
+			if (deliveriesIds != null && deliveriesIds.Length > 0)
+			{
+
+			}
+			if (ouputsIds != null && ouputsIds.Length > 0)
+			{
+				
+
+			}
+			using (SqlConnection conn = new SqlConnection(AppSettings.GetConnectionString(this, Consts.ConnectionStrings.StagingDatabase)))
+			{
+				SqlTransaction tran = null;
+				SqlCommand cmd = null;
+				conn.Open();
+				if (deliveriesIds != null && deliveriesIds.Length > 0)
+				{
+					tran = conn.BeginTransaction();
+					cmd = DataManager.CreateCommand(spRolebackbyDeliveries, System.Data.CommandType.StoredProcedure);
+					cmd.Connection = conn;
+					cmd.Transaction = tran;
+
+					foreach (string deliveryID in deliveriesIds)
+					{
+
+						cmd.Parameters["@DeliveryID"].Value = deliveryID;
+						cmd.Parameters["@TableName"].Value = tableName;
+						cmd.ExecuteNonQuery();
+					}
+				}
+				if (ouputsIds != null && ouputsIds.Length > 0)
+				{
+
+					cmd = DataManager.CreateCommand(spRolebackbyOutputs, System.Data.CommandType.StoredProcedure);
+					cmd.Connection = conn;
+					cmd.Transaction = tran;
+
+					foreach (string outputID in ouputsIds)
+					{
+
+						cmd.Parameters["@DeliveryOutputID"].Value = outputID;
+						cmd.Parameters["@TableName"].Value = tableName;
+						cmd.ExecuteNonQuery();
+					}
+				}
+
+				if (tran != null)
+					tran.Commit();
+
+
+			}
+
+
+			return Core.Services.ServiceOutcome.Success; */
 		}
 
 
