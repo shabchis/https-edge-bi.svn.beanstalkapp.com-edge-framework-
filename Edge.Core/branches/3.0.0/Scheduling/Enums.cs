c1727cfe13811a56ef2aa9e501bd1cb0cb7e4619
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2012-08-26 08:58:51 +0000

    Done fixing scheduling changes

---
+++ Edge.Core/branches/3.0.0/Scheduling/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Edge.Core.Services
+{
+	public enum SchedulingStatus
+	{
+		New = 0,
+		WaitingForSchedule = 1,
+		Scheduled = 2,
+		Activated = 3,
+		CouldNotBeScheduled = 4
+	}
+}
