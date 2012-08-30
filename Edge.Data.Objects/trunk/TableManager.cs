a9abd8fed3c211f0cbfd6a0b1cb70849bfbffa13
alonyaari <alonyaari@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2012-08-30 07:45:42 +0000

    REMOVE TARRGETS FROM AD START TABLE MANAGER

---
+++ Edge.Data.Objects/trunk/TableManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Objects.Objects;
+using System.Reflection;
+
+namespace Edge.Data.Objects
+{
+	class TableManager
+	{
+		Dictionary<string, object> objects;
+		Dictionary<string, Column> cols;
+		public List<string> GetColumnsList(MetricsUnit metricsUnit)
+		{
+			List<string> columns = new List<string>();
+			GetAllColumns(metricsUnit);
+
+
+
+
+			return columns;
+
+		}
+		public void GetAllColumns(object obj)
+		{
+			if (obj.GetType().BaseType == typeof(MetricsUnit))
+			{
+
+				if (obj.GetType() == typeof(AdMetricsUnit))
+				{
+					//TODO:GET ALL OTHER PROPERTIS
+					GetAllColumns(((AdMetricsUnit)obj).Ad);
+				}
+				else
+				{		
+					//TODO:GET ALL OTHER PROPERTIS
+					foreach (KeyValuePair<MetaProperty, object> propertyDimension in ((GenericMetricsUnit)obj).PropertyDimensions)
+					{
+						GetAllColumns(propertyDimension);
+					}
+				}
+			}
+			else if (obj.GetType().BaseType == typeof(EdgeObject))
+			{
+				//TODO: ADD TYPE NAME AS COLUMN
+				foreach (FieldInfo field in obj.GetType().BaseType.GetFields()) //only edgeobject properties
+				{
+					GetAllColumns(field.GetValue(obj));					
+				}
+				foreach (FieldInfo field in obj.GetType().GetFields(BindingFlags.DeclaredOnly))
+				{										
+						GetAllColumns((EdgeObject)field.GetValue(obj));					
+				}
+			}
+			else
+			{
+				//ADD FIELD
+
+			}
+
+		}
+		private void AddColumn(FieldInfo field)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
