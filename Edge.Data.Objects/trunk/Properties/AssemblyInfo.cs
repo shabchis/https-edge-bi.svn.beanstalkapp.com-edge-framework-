9daa2c6df0686bf6f00527cd58ccaaef83593c24
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2012-09-03 16:25:43 +0000

    Fixing pipeline of 3.0.0

--- Edge.Data.Objects/trunk/Properties/AssemblyInfo.cs
+++ Edge.Data.Objects/trunk/Properties/AssemblyInfo.cs
@@ -32,5 +32,10 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+<<<<<<< .mine
+[assembly: AssemblyVersion("3.0.0.731")]
+[assembly: AssemblyFileVersion("3.0.0.731")]
+=======
 [assembly: AssemblyVersion("3.0.0.735")]
 [assembly: AssemblyFileVersion("3.0.0.735")]
+>>>>>>> .r745
