a93838262910426b1ef574a589164cde8a76daf6
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2012-09-05 14:54:59 +0000

    Integrating Eggplant

---
+++ Edge.Data.Pipeline.Metrics/branches/3.0.0/Base/Submanagers/ObjectManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Edge.Data.Objects;
+
+/*
+namespace Edge.Data.Pipeline.Metrics
+{
+	internal class ObjectManager
+	{
+		static ObjectManager()
+		{
+			var ad_outbound = new OutboundMapping<Ad>()
+		}
+	}
+}
+*/
