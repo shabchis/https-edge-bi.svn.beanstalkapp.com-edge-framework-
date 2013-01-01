9b321899bb4a1adf0a3cb74c3cf684fe245d3e3d
shirat <shirat@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-01-01 08:36:32 +0000

    Pipeline and Pipeline metrics projects fixes and refactoring - pass compilation!

---
+++ Edge.Data.Pipeline/branches/3.0.0/Objects/MetricsUnit.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Edge.Data.Objects;
+
+namespace Edge.Data.Pipeline.Objects
+{
+	public abstract class MetricsUnit
+	{	
+		public DateTime TimePeriodStart;
+		public DateTime TimePeriodEnd;
+
+		public Currency Currency;
+
+		public List<TargetMatch> TargetDimensions;
+		public Dictionary<Measure, double> MeasureValues;
+
+		public DeliveryOutput Output;
+
+		public abstract IEnumerable<EdgeObject> GetObjectDimensions();
+	}
+
+	public class AdMetricsUnit: MetricsUnit
+	{
+		public Ad Ad;
+
+		public override IEnumerable<EdgeObject> GetObjectDimensions()
+		{
+			yield return Ad;
+			foreach (TargetMatch target in TargetDimensions)
+				yield return target;
+		}
+	}
+
+	public class GenericMetricsUnit : MetricsUnit
+	{
+		public Channel Channel;
+		public Account Account;
+
+		public Dictionary<ConnectionDefinition, object> PropertyDimensions;
+
+		public override IEnumerable<EdgeObject> GetObjectDimensions()
+		{
+			foreach (var prop in PropertyDimensions)
+				if (prop.Value is EdgeObject)
+					yield return (EdgeObject)prop.Value;
+
+			foreach (TargetMatch target in TargetDimensions)
+				yield return target;
+		}
+	}
+}
