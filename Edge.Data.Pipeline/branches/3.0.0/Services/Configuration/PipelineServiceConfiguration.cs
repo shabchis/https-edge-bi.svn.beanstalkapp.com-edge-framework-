4d9ee27db85ae89c5d95846214de1dd30f637c28
shirat <shirat@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-01-07 16:11:39 +0000

    Processor changes

--- Edge.Data.Pipeline/branches/3.0.0/Services/Configuration/PipelineServiceConfiguration.cs
+++ Edge.Data.Pipeline/branches/3.0.0/Services/Configuration/PipelineServiceConfiguration.cs
@@ -15,7 +15,11 @@ namespace Edge.Data.Pipeline.Services
 		public DateTimeRange? TimePeriod { get { return _timePeriod; } set { EnsureUnlocked(); _timePeriod = value; } }
 
 		DeliveryConflictBehavior? _conflictBehavior;
-		public DeliveryConflictBehavior? ConflictBehavior { get { return _conflictBehavior; } set { EnsureUnlocked(); _conflictBehavior = value; } } 
+		public DeliveryConflictBehavior? ConflictBehavior { get { return _conflictBehavior; } set { EnsureUnlocked(); _conflictBehavior = value; } }
+
+		private string _mappingConfigPath;
+		public string MappingConfigPath { get { return _mappingConfigPath; } set { EnsureUnlocked(); _mappingConfigPath = value; } }
+
 		#endregion
 
 		#region Ctors
@@ -32,6 +36,7 @@ namespace Edge.Data.Pipeline.Services
 			info.AddValue("_deliveryID", _deliveryID);
 			info.AddValue("_timePeriod", _timePeriod);
 			info.AddValue("_conflictBehavior", _conflictBehavior);
+			info.AddValue("_mappingConfigPath", _mappingConfigPath);
 		}
 
 		protected override void Deserialize(SerializationInfo info, StreamingContext context)
@@ -40,6 +45,7 @@ namespace Edge.Data.Pipeline.Services
 			_deliveryID = (Guid?)info.GetValue("_deliveryID", typeof(Guid?));
 			_timePeriod = (DateTimeRange?)info.GetValue("_timePeriod", typeof(DateTimeRange?));
 			_conflictBehavior = (DeliveryConflictBehavior?)info.GetValue("_conflictBehavior", typeof(DeliveryConflictBehavior?));
+			_mappingConfigPath = (string)info.GetValue("_mappingConfigPath", typeof(string));
 		}
 
 		protected override void CopyConfigurationData(ServiceConfiguration sourceConfig, ServiceConfiguration targetConfig)
@@ -55,6 +61,7 @@ namespace Edge.Data.Pipeline.Services
 			targetc._deliveryID = sourcec._deliveryID ?? targetc._deliveryID;
 			targetc._timePeriod = sourcec._timePeriod ?? targetc._timePeriod;
 			targetc._conflictBehavior = sourcec._conflictBehavior ?? targetc._conflictBehavior;
+			targetc._mappingConfigPath = sourcec._mappingConfigPath ?? sourcec._mappingConfigPath;
 		} 
 		#endregion
 	}
