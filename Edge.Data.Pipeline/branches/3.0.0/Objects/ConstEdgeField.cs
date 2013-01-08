8d27c3f82a14f7e252409536140ac6a96511522a
shirat <shirat@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-01-08 14:17:21 +0000

    Import metrics - now working with GenericMetricsUnit not including Import objects

---
+++ Edge.Data.Pipeline/branches/3.0.0/Objects/ConstEdgeField.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Edge.Data.Pipeline.Objects
+{
+	public class ConstEdgeField
+	{
+		public string Name { get; set; }
+		public object Value { get; set; }
+		public Type Type { get; set; }
+	}
+}
