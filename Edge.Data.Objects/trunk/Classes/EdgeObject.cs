1bb8c07105dbc1ffb7ad2a562f13e887bf73e71e
shirat <shirat@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-01-15 15:03:14 +0000

    Import edge objects - 1st draft (simple dimention)

--- Edge.Data.Objects/trunk/Classes/EdgeObject.cs
+++ Edge.Data.Objects/trunk/Classes/EdgeObject.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Edge.Data.Objects
 {
-	public abstract partial class EdgeObject : EdgeObjectBase
+	public partial class EdgeObject : EdgeObjectBase
 	{
 		public long GK;
 		public Account Account;
 		public EdgeType EdgeType;
 		
 		public Dictionary<ExtraField, object> ExtraFields;
+
+		public string TK { get { return ToString(); } }
+
+		public override string ToString()
+		{
+			var str = EdgeType.Name;
+			foreach (var field in ExtraFields)
+			{
+				str = String.Format("{0}{1}{2}", str, str.Length > 0 ? "_" : String.Empty, field.Value);
+			}
+			return str;
+		}
 	}
 	
 	public abstract class EdgeObjectBase
@@ -26,6 +36,4 @@ namespace Edge.Data.Objects
 			return null;
 		}
 	}
-
-
 }
