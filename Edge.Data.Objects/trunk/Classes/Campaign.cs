543199a541293f7628359cb27c152c0072cb2623
shirat <shirat@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-01-16 13:24:20 +0000

    More changes to support real processor scenario

--- Edge.Data.Objects/trunk/Classes/Campaign.cs
+++ Edge.Data.Objects/trunk/Classes/Campaign.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Edge.Data.Objects
+﻿namespace Edge.Data.Objects
 {
 	public partial class Campaign : ChannelSpecificObject
 	{
 		public double Budget;
-	}
+		public string Name;
 
+		public override string TK { get {return string.Format("Campaign_Name:{0}_Budget{1}", Name, Budget); }}
+	}
 }
