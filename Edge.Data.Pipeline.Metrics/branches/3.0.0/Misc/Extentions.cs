e78fe49068e53db4a6441742586ecd157a5a8d0e
shirat <shirat@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-01-30 11:19:36 +0000

    Supporting Composite parts collections

---
+++ Edge.Data.Pipeline.Metrics/branches/3.0.0/Misc/Extentions.cs
@@ -0,0 +1,13 @@
+﻿namespace Edge.Data.Pipeline.Metrics.Misc
+{
+	#region Extensions
+	public static class StringExtenstions
+	{
+		public static string RemoveInvalidCharacters(this string expression)
+		{
+			return expression.Replace("'", "");
+		}
+	}
+	
+	#endregion
+}
