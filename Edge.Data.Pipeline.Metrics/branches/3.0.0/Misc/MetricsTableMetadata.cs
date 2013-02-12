d2733e929d4038cbeb05cc183583661108d478be
shirat <shirat@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-02-12 09:48:58 +0000

    Metrics table metadata to store edge fields and measures of Metrics table structure

--- Edge.Data.Pipeline.Metrics/branches/3.0.0/Misc/MetricsTableMetadata.cs
+++ Edge.Data.Pipeline.Metrics/branches/3.0.0/Misc/MetricsTableMetadata.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections.Generic;
+using Edge.Data.Objects;
 
 namespace Edge.Data.Pipeline.Metrics.Misc
 {
+	/// <summary>
+	/// Metadata of the metrics table which contains table name and list of fields in table structure
+	/// each field can be EdgeField or Measure
+	/// </summary>
 	public class MetricsTableMetadata
 	{
+		public string TableName { get; set; }
 		public List<FieldMetadata> FieldList { get; set; }
 
 		public MetricsTableMetadata()
@@ -12,10 +18,51 @@ namespace Edge.Data.Pipeline.Metrics.Misc
 		}
 	}
 
+	/// <summary>
+	/// Table field metadata, can be EdgeField or Measure
+	/// Contains various access properties for easy use (such as ID, Name, etc.)
+	/// </summary>
 	public struct FieldMetadata
 	{
-		public string FieldName { get; set; }
-		public int FieldId { get; set; }
-		public bool IsMeasure { get; set; }
+		public object Field { get; set; }
+
+		public EdgeField EdgeField
+		{
+			get { return Field as EdgeField; }
+		}
+		public Measure Measure
+		{
+			get { return Field as Measure; }
+		}
+		public bool IsMeasure
+		{
+			get { return Field is Measure; }
+		}
+		public bool IsEdgeField
+		{
+			get { return Field is EdgeField; }
+		}
+		public string FieldName
+		{
+			get 
+			{ 
+				return	IsMeasure ? Measure.Name :
+						IsEdgeField ? EdgeField.Name :
+						string.Empty;
+			}
+		}
+		public int FieldId
+		{
+			get
+			{
+				return IsMeasure ? Measure.ID :
+						IsEdgeField ? EdgeField.FieldID :
+						0;
+			}
+		}
+
+		//public string FieldName { get; set; }
+		//public int FieldId { get; set; }
+		//public bool IsMeasure { get; set; }
 	}
 }
