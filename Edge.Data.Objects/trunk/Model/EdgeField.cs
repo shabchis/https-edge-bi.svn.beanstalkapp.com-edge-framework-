679bca1c2ebac2bd5af1f7d440fe7cbe294747de
shirat <shirat@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-02-18 16:36:37 +0000

    Changes in Pipeline and Pipeline Metrics to support many2many relationship between EdgeType and EdgeField

--- Edge.Data.Objects/trunk/Model/EdgeField.cs
+++ Edge.Data.Objects/trunk/Model/EdgeField.cs
@@ -15,9 +15,9 @@ namespace Edge.Data.Objects
 			public static EntityProperty<EdgeField, string> Name = new EntityProperty<EdgeField, string>("Name");
 			public static EntityProperty<EdgeField, string> DisplayName = new EntityProperty<EdgeField, string>("DisplayName");
 			public static EntityProperty<EdgeField, EdgeType> FieldEdgeType = new EntityProperty<EdgeField, EdgeType>("FieldEdgeType");
-			public static EntityProperty<EdgeField, EdgeType> ParentEdgeType = new EntityProperty<EdgeField, EdgeType>("ParentEdgeType");
-			public static EntityProperty<EdgeField, string> ColumnPrefix = new EntityProperty<EdgeField, string>("ColumnPrefix");
-			public static EntityProperty<EdgeField, int> ColumnIndex = new EntityProperty<EdgeField, int>("ColumnIndex");
+			//public static EntityProperty<EdgeField, EdgeType> ParentEdgeType = new EntityProperty<EdgeField, EdgeType>("ParentEdgeType");
+			//public static EntityProperty<EdgeField, string> ColumnPrefix = new EntityProperty<EdgeField, string>("ColumnPrefix");
+			//public static EntityProperty<EdgeField, int> ColumnIndex = new EntityProperty<EdgeField, int>("ColumnIndex");
 		}
 	}
 }
