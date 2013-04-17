384c09f5e536c1b28192d769bec3347065f68e59
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-04-17 07:13:30 +0000

    Working on outbound data (including batches), not compiling yet, breaking changes

---
+++ Eggplant.Entities/branches/wip/Queries/QueryResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Eggplant.Entities.Queries
+{
+	public class QueryResponse
+	{
+	}
+
+	public class QueryResponse<T>
+	{
+		public Dictionary<string, QueryOutput> Outputs { get; private set; }
+		public IEnumerable<T> Results { get; private set; }
+	}
+}
