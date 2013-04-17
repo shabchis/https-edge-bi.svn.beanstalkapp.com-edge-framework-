1441a1b76facd035801cc04fa435ab963ff42dbb
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-04-17 07:13:30 +0000

    Working on outbound data (including batches), not compiling yet, breaking changes

---
+++ Eggplant.Entities/branches/wip/Persistence/SqlServer/SqlBulkAction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlClient;
+
+namespace Eggplant.Entities.Persistence.SqlServer
+{
+	public class SqlBulkAction: PersistenceAction
+	{
+		public string TableName {get; set;}
+		public int BatchSize {get; set;}
+		public SqlBulkCopyOptions BulkCopyOptions { get; set; }
+
+		public new SqlBulkAction(string tableName, int batchSize, SqlBulkCopyOptions bulkCopyOptions = SqlBulkCopyOptions.Default)
+		{
+			this.TableName = tableName;
+			this.BatchSize = batchSize;
+			this.BulkCopyOptions = bulkCopyOptions;
+		}
+
+		public override bool IsAppendable
+		{
+			get { return false; }
+		}
+
+		protected override void OnAppend(PersistenceAction action)
+		{
+			throw new NotSupportedException("Appending bulk actions is not supported.");
+		}
+
+		public override PersistenceAction Clone()
+		{
+			return new SqlBulkAction(this.TableName, this.BatchSize, this.BulkCopyOptions);
+		}
+
+		public override PersistenceAdapter GetAdapter()
+		{
+			return new SqlBulkAdapter(null, this);
+		}
+	}
+}
---
+++ Eggplant.Entities/branches/wip/Persistence/SqlServer/SqlBulkAdapter.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Eggplant.Entities.Persistence.SqlServer
+{
+	public class SqlBulkAdapter: PersistenceAdapter
+	{
+		SqlBulkCopy _bulk;
+		DataTable _buffer;
+
+		internal SqlBulkAdapter(SqlPersistenceConnection connection, SqlBulkAction action) :base(connection, action)
+		{
+			_bulk = new SqlBulkCopy(connection.DbConnection, action.BulkCopyOptions, null)
+			{
+				BatchSize = action.BatchSize,
+				DestinationTableName = action.TableName
+			};
+
+			_buffer = new DataTable("SqlBulkAdapter._buffer");
+			foreach (PersistenceParameter param in action.Parameters.Values)
+			{
+				var options = (SqlPersistenceParameterOptions)param.Options;
+				var tableCol = new DataColumn(param.Name);
+				if (options.Size != null)
+					tableCol.MaxLength = options.Size.Value;
+				_buffer.Columns.Add(tableCol);
+				_bulk.ColumnMappings.Add(new SqlBulkCopyColumnMapping(param.Name, param.Name));
+			}
+		}
+
+		public override bool IsReusable
+		{
+			get { throw new NotImplementedException(); }
+		}
+
+		public override bool HasField(string field)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override object GetField(string field)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void SetField(string field, object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool HasParam(string param)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override object GetParam(string param)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void SetParam(string param, object value)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void ExecuteAction()
+		{
+			// TODO: only execute if Flush should be called now
+
+			throw new NotImplementedException();
+		}
+
+		public override bool NextResultSet()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override bool NextResult()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override void Dispose()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
