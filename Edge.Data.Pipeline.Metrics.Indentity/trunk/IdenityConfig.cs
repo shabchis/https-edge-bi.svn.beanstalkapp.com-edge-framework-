6f187cbe9bf7c6bf9ae6cb7af8dd0dfed5fa2f66
shirat <shirat@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-07-28 07:40:15 +0000

    Delivery Pipeleine: Google settings service (Campaign location and language)

---
+++ Edge.Data.Pipeline.Metrics.Indentity/trunk/IdenityConfig.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Edge.Data.Pipeline.Metrics.Indentity
+{
+	[XmlRoot(ElementName = "IdentityConfig")]
+	public class IdentityConfig
+	{
+		[XmlAttribute(AttributeName = "CreateNewObjects")]
+		public bool CreateNewObjects { get; set; }
+
+		[XmlAttribute(AttributeName = "UpdateExistingObjects")]
+		public bool UpdateExistingObjects { get; set; }
+		
+		[XmlElement("EdgeType")]
+		public List<EdgeTypeConfig> EdgeTypes { get; set; }
+
+		public IdentityConfig()
+		{
+			CreateNewObjects = true;
+			UpdateExistingObjects = true;
+			EdgeTypes = new List<EdgeTypeConfig>();
+		}
+
+		#region Serialization
+		public static string Serialize(IdentityConfig config)
+		{
+			var ser = new XmlSerializer(typeof(IdentityConfig));
+			using (var textWriter = new StringWriter())
+			{
+				ser.Serialize(textWriter, config);
+				return textWriter.ToString();
+			}
+		}
+
+		public static IdentityConfig Deserialize(string xml)
+		{
+			var ser = new XmlSerializer(typeof(IdentityConfig));
+			using (var reader = new StringReader(xml))
+			{
+				return ser.Deserialize(reader) as IdentityConfig;
+			}
+		} 
+		#endregion
+	}
+
+	public class EdgeTypeConfig
+	{
+		[XmlAttribute(AttributeName = "Name")]
+		public string Name { get; set; }
+
+		[XmlAttribute(AttributeName = "CreateNewObjects")]
+		public bool CreateNewObjects { get; set; }
+
+		[XmlAttribute(AttributeName = "UpdateExistingObjects")]
+		public bool UpdateExistingObjects { get; set; }
+		
+		[XmlElement("FieldToUpdate")]
+		public List<FieldConfig> Fields { get; set; }
+
+		public EdgeTypeConfig()
+		{
+			CreateNewObjects = true;
+			UpdateExistingObjects = true;
+			Fields = new List<FieldConfig>();
+		}
+
+		public string GetFieldList()
+		{
+			var fieldStr = new StringBuilder();
+			foreach (var field in Fields)
+			{
+				fieldStr.AppendFormat("{0},", field.Name);
+			}
+			return fieldStr.Length > 0 ? fieldStr.Remove(fieldStr.Length - 1, 1).ToString() : string.Empty;
+		}
+	}
+
+	public class FieldConfig
+	{
+		[XmlAttribute(AttributeName = "Name")]
+		public string Name { get; set; }
+	}
+}
