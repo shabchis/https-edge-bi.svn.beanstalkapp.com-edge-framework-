8784a93b5273d1e208cf93ae5241a691f7e0d5df
shirat <shirat@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-07-29 06:50:14 +0000

    Delivery Pipeline: support + configuration for Google GEO report

--- Edge.Data.Objects/trunk/Classes/Location.cs
+++ Edge.Data.Objects/trunk/Classes/Location.cs
@@ -1,9 +1,25 @@
-﻿namespace Edge.Data.Objects
+﻿using System;
+using System.Collections.Generic;
+namespace Edge.Data.Objects
 {
 	public class Location : EdgeObject
 	{
 		public string Name { get; set; }
 		public LocationType LocationType { get; set; }
+
+		public override IEnumerable<ObjectDimension> GetObjectDimensions()
+		{
+			foreach (var dimension in base.GetObjectDimensions())
+			{
+				yield return dimension;
+			}
+
+			if (LocationType != null) yield return new ObjectDimension
+			{
+				Field = EdgeType["LocationType"],
+				Value = LocationType
+			};
+		}
 	}
 
 	public class LocationType : StringValue {}
