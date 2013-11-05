50928a0f6bb5a6f0a864f9c85f3b03f53f2cd4c0
doronassayas <doronassayas@8053b154-6d14-4ee5-98de-fcb32efc8e66>
2013-11-05 17:07:10 +0000

    More TempScheduler changes

---
+++ Edge.Core.Scheduling/trunk/SchedulerState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Edge.Core.Scheduling
+{
+	public enum SchedulerState
+	{
+		Stopped = 0,
+		Starting = 1,
+		Started = 2,
+		Stopping = 3
+	}
+}
